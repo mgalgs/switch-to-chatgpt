82608361a26b907c6a69349a9d84b8e4adc7181e
Mitchel Humpherys <mitch.special@gmail.com>
2025-03-03 11:34:13 -0800

    extension: Update ChatGPT URL

--- extension/background.js
+++ extension/background.js
@@ -2,7 +2,7 @@ browser.commands.onCommand.addListener((command) => {
     if (command === "switch-tab") {
         browser.tabs.query({}).then((tabs) => {
             for (let tab of tabs) {
-                if (tab.url.includes("https://chat.openai.com/")) {
+                if (tab.url.includes("https://chatgpt.com/")) {
                     browser.tabs.update(tab.id, { active: true });
                     return;
                 }
